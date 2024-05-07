9fd5c87422a18c88580b1cb7b104fa75d1ebd44f
rammelion <kereszthidi.g@rammelion.hu>
2024-05-07 18:19:44 +0200

    Temporary Google Tag Fix

--- resources/views/components/cookies/container.blade.php
+++ resources/views/components/cookies/container.blade.php
@@ -1,17 +1,4 @@
-<script>
-    // Define dataLayer and the gtag function.
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
 
-    // Set default consent to 'denied' as a placeholder
-    // Determine actual values based on your own requirements
-    gtag('consent', 'default', {
-    'ad_storage': 'denied',
-    'ad_user_data': 'denied',
-    'ad_personalization': 'denied',
-    'analytics_storage': 'denied'
-    });
-</script>
 
 @php
     $functionalityCookie = config('cookies_consent.cookie_prefix')  . 'cookies_consent_functionality';
